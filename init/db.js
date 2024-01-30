28df85ad59eb8d6cfd0561e3f994f4e3b9a39e89
Yash Varma <yashvarma2702@gmail.com>
2024-01-31 00:20:42 +0530

    KeyFile

--- init/db.js
+++ init/db.js
@@ -1,4 +1,7 @@
 const {google} = require("googleapis");
+const keyFile = require("./credentials.json");
+
+console.log(keyFile)
 
 module.exports = async () => {
     const auth = new google.auth.GoogleAuth({
